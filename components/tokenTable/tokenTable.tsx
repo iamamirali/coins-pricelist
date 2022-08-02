6972eb8d8b8e07705b8f00932a7f9db1c1c71031
iamamirali <iamamirali.sh@gmail.com>
2022-08-02 13:41:20 +0430

    add token name with crypto-symbol

--- components/tokenTable/tokenTable.tsx
+++ components/tokenTable/tokenTable.tsx
@@ -1,24 +1,33 @@
 import useSocketData from "hooks/useSocketData";
 import { useSelector } from "react-redux";
 import { RootState } from "store/models/RootState.model";
+import { cryptoSymbol } from "crypto-symbol";
 import { WebsocketData } from "store/models/Websocket.model";
 import TableStyles from "./tokenTable.module.scss";
+
+const { nameLookup } = cryptoSymbol({});
 
 function TokenTable() {
   const socketData = useSelector((state: RootState) => state.socket.data);
   const [tokenData] = useSocketData(socketData);
 
-  function showTokenSymbol(token: string) {
+  function getTokenSymbol(token: string) {
     return token.split("USDT")[0];
   }
 
   function showCryptoList() {
     return tokenData.map((item: WebsocketData) => {
       const { p, s } = item;
+      const tokenSymbol = getTokenSymbol(s);
+      const tokenName = nameLookup(tokenSymbol);
+
       return (
         <tr key={s}>
-          <td>{showTokenSymbol(s)}</td>
-          <td>{+p}</td>
+          <td>
+            {tokenName}{" "}
+            <span className={TableStyles.extraTitle}>{tokenSymbol}</span>
+          </td>
+          <td>${+p}</td>
         </tr>
       );
     });
@@ -28,9 +37,9 @@ function TokenTable() {
     <table className={TableStyles.table}>
       <thead className={TableStyles.head}>
         <tr>
-          <th>Token</th>
+          <th>Token Name</th>
           <th>
-            Price <span className={TableStyles.currency}>(USDT)</span>
+            Price <span className={TableStyles.extraTitle}>(USDT)</span>
           </th>
         </tr>
       </thead>
